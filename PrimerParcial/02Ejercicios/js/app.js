819315c57a041af91c904dff0affcd6c859361db
Hu1tzil <dperezg2406@alumno.ipn.mx>
2026-09-23 01:07:04 -0600

    Se agrega ej. arreglos 1 y appjs ya terminados

---
+++ PrimerParcial/02Ejercicios/js/app.js
@@ -0,0 +1,67 @@
+
+
+const talleres = [
+  { nombre: 'Introducción a Python', instructor: 'Ing. María López', cupo: 25, inscritos: 25 },
+  { nombre: 'Fundamentos de Redes', instructor: 'Ing. Carlos Ramírez', cupo: 30, inscritos: 18 },
+  { nombre: 'Diseño de Bases de Datos', instructor: 'Ing. Ana Torres', cupo: 20, inscritos: 20 },
+  { nombre: 'Desarrollo Web con JS', instructor: 'Ing. María López', cupo: 25, inscritos: 10 },
+];
+
+
+function pintarTabla(){
+    //debe de obtener la tabla y rellenarla con los datos de talleres
+    const table = document.getElementById('tabla-talleres');
+    table.innerHTML = '';
+    talleres.forEach((t) => {
+        const row = document.createElement('tr');
+        row.innerHTML = ` 
+          <td>${t.nombre}</td>
+          <td>${t.inscritos}</td>
+          <td>${t.cupo}</td>
+          <td>${t.instructor}</td>
+          `;
+          
+          table.appendChild(row);
+    });
+}
+
+
+const formArreglos = document.getElementById('form-arreglos');
+const resultadoArreglos = document.getElementById('resultado-arreglo');
+const selectOperacionArreglo = document.getElementById('operacion-arreglo');
+
+formArreglos.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+    const operacion = selectOperacionArreglo.value;
+
+    let resultado;
+
+    switch(operacion){
+        case 'forEach':
+            resultado = talleres.map((t) => `- ${t.nombre} (${t.inscritos}/${t.cupo})`).join('\n');
+            break;
+        case 'map': 
+            resultado = talleres.map((t) => t.nombre).join(', ');
+            break;
+        case 'filter': 
+            resultado = talleres.filter((t) => t.inscritos >= t.cupo).join(', ');
+            break;
+        case 'find':
+            resultado = talleres.find((t) => t.instructor === 'Ing. maría Lópex');
+            break;
+        case 'reduce':
+            resultado = talleres.reduce((total, t) => total + t.inscritos, 0);
+            break;
+        case 'filterMap':
+            resultado = talleres.filter((t) => t.inscritos < t.cupo).map((t) => t.nombre).join(', ');
+            break;
+        default:
+            resultado = 'Operación no válida';
+        }
+        
+       
+       
+
+
+    resultadoArreglos.textContent = resultado;
+});
